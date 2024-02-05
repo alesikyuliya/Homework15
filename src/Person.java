f42c9aa3ff5673dbd60c67460ee71ba5a2bbc597
Yuliya Alesik <alesikyuliya@outlook.com>
2024-02-05 23:41:19 +0100

    First Commit

---
+++ src/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+
+    public String personInfo(String name, String surname, String city, long phone){
+        return "Call mr " + name + " " + surname + " from the city " + city + " with the phone number " + phone;
+    }
+
+}
